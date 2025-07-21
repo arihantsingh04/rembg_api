d142c2c718a2e0978949326ce42a2992feb7296e
Arihant Singh <arihants2004@gmail.com>
2025-07-21 22:15:40 +0530

    added

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+    if 'image' not in request.files:
+        return {'error': 'No image uploaded'}, 400
+
+    image_file = request.files['image']
+    input_image = Image.open(image_file.stream).convert("RGBA")
+    output_image = remove(input_image)
+
+    img_io = io.BytesIO()
+    output_image.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
